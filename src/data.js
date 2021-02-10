b3407cbc9b26919719080826d330e8a18bce6230
matt yao <myao.frontend@gmail.com>
2021-02-09 22:55:50 -0500

    staged

---
+++ src/data.js
@@ -0,0 +1,94 @@
+ const transactions = [
+    {
+        id: "1",
+        name: "John",
+        date: "2020-11-01",
+        money: "120"
+    },
+    {
+        id: "1",
+        name: "John",
+        date: "2020-11-08",
+        money: "201"
+    },
+    {
+        id: "1",
+        name: "John",
+        date: "2020-11-03",
+        money: "94"
+    },
+    {
+        id: "1",
+        name: "John",
+        date: "2020-11-23",
+        money: "131"
+    },
+    {
+        id: "1",
+        name: "John",
+        date: "2020-12-23",
+        money: "317"
+    },
+    {
+        id: "2",
+        name: "Rose",
+        date: "2020-11-07",
+        money: "231"
+    },
+    {
+        id: "2",
+        name: "Rose",
+        date: "2020-12-01",
+        money: "1031"
+    },
+    {
+        id: "2",
+        name: "Rose",
+        date: "2020-12-30",
+        money: "23"
+    },
+    {
+        id: "2",
+        name: "Rose",
+        date: "2020-12-15",
+        money: "421"
+    },
+    {
+        id: "1",
+        name: "John",
+        date: "2020-10-03",
+        money: "82"
+    },
+    {
+        id: "3",
+        name: "David",
+        date: "2020-10-21",
+        money: "2222"
+    },
+    {
+        id: "3",
+        name: "David",
+        date: "2020-11-01",
+        money: "230"
+    },
+    {
+        id: "3",
+        name: "David",
+        date: "2020-12-20",
+        money: "1200"
+    },
+    {
+        id: "3",
+        name: "David",
+        date: "2020-11-02",
+        money: "35"
+    },
+    {
+        id: "3",
+        name: "David",
+        date: "2020-12-28",
+        money: "641"
+    },
+];
+
+export default transactions;
